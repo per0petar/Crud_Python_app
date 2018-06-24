6cf2b38f585337a145d81afce2f730a19bc1ecab
Petar <per0_petar@hotmail.com>
2018-06-24 19:54:13 +0200

    added "login" app folder and made some changes in files

---
+++ CRUD_Python_app/login/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from datetime import datetime
+# Create your models here.
+
+
+class Users(models.Model):
+    user_name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.user_name
